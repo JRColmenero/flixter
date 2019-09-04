4e89d37ae128404d869668b16cc8a9c452e14e91
Rebekah Colmenero <jrebekahb@gmail.com>
2019-09-04 01:03:43 +0000

    Initial commit

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,12 +1,21 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_enrollment_for_current_lesson, only: [:show]
+
   def show
-    before_action :authenticate_user!
   end
 
-  private
+private
+
+ def require_enrollment_for_current_lesson
+    if current_user.enrolled_in?(current_lesson.section.course) != true
+      redirect_to course_path(current_lesson.section.course), alert: 'Enroll to View' 
+    end
+  end
 
   helper_method :current_lesson
   def current_lesson
     @current_lesson ||= Lesson.find(params[:id])
   end
+
 end
